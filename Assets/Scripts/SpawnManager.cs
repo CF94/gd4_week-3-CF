5e1cc340bfaf29ab9fb9075f6afdef8002e00487
CF94 <45069420+CF94@users.noreply.github.com>
2024-12-06 03:54:07 +0000

    0.2

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -2,38 +2,40 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public GameObject player;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created    
     public GameObject projectileBananna;
     public GameObject projectileCarrot;
     public GameObject projectileCookie;
+    float carrotSpeed = 0.5f;
+    float cookieSpeed = 0.1f;
     float time;
-    public float fireRate = 1f;
     void Start()
     {
         time= Time.time;
     }
     // Update is called once per frame
     void Update()
-    {
+    {        
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            Instantiate(projectileBananna, transform.position, Quaternion.identity);
+            Instantiate(projectileBananna, transform.position, transform.rotation);
         }
         if (Input.GetKey(KeyCode.Alpha2))
         {
-            if (Time.time - time > fireRate)
+            if (Time.time - time > carrotSpeed)
             {
-                Instantiate(projectileCarrot, transform.position, Quaternion.identity);
+                Instantiate(projectileCarrot, transform.position, transform.rotation);
                 time = Time.time;
-            }        
-            
+            }                
         }
-        if (Input.GetKeyDown(KeyCode.Alpha3))
+        if (Input.GetKey(KeyCode.Alpha3))
         {
-            Instantiate(projectileCookie, transform.position, Quaternion.identity);
+            if (Time.time - time > cookieSpeed)
+            {
+                Instantiate(projectileCookie, transform.position, transform.rotation);
+                time = Time.time;
+            }               
         }
-    }    
-    
+    }
 
 }
